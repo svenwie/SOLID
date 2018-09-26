0aabdb7a66349cc809a58e08aa2e92de4c913588
svenw <herrvonboedefeld1@web.de>
2018-09-26 14:27:28 +0200

    The SOLID Principles for training purpose

---
+++ SingleRespDesignConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardMessages.WelcomeMessage();
+
+            Person user = PersonDataCapture.Capture();
+
+            bool isUserValid = PersonValidator.Validate(user);
+
+            if (isUserValid == false)
+            {
+                StandardMessages.EndApplication();
+                return;
+            }
+            else
+            {
+                AccountGenerator.CreateAccount(user);
+
+                StandardMessages.EndApplication();
+            }
+        }
+    }
+}
